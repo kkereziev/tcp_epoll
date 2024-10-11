10ef09fb3e74959105a0b1ac29c12711f67d3ee9
kkereziev <kkereziev97@gmail.com>
2024-10-11 11:22:33 +0300

    feat: add epoll event loop

--- src/main.zig
+++ src/main.zig
@@ -13,20 +13,35 @@ pub fn main() !void {
     var tcp_server = try server_addr.listen(.{});
     defer tcp_server.deinit();
 
+    // while (true) {
+    //     std.debug.print("waiting for conn\n", .{});
+    //     const conn = try tcp_server.accept();
+    //
+    //     echo(conn) catch {
+    //         std.debug.print("closed conn\n", .{});
+    //     };
+    //
+    //     std.debug.print("bye bye\n", .{});
+    // }
+
+    const epoll_fd = try std.posix.epoll_create1(0);
+    var listen_event = std.os.linux.epoll_event{
+        .events = std.os.linux.EPOLL.IN,
+        .data = .{ .fd = tcp_server.stream.handle },
+    };
+
+    try std.posix.epoll_ctl(epoll_fd, std.os.linux.EPOLL.CTL_ADD, tcp_server.stream.handle, &listen_event);
+
+    var events: [100]std.os.linux.epoll_event = undefined;
+
     while (true) {
-        std.debug.print("waiting for conn\n", .{});
-        const conn = try tcp_server.accept();
+        const num_fd = std.posix.epoll_wait(epoll_fd, &events, -1);
 
-        echo(conn) catch {
-            std.debug.print("closed conn\n", .{});
-        };
+        for (events[0..num_fd]) |event| {
+            std.debug.assert(event.data.fd == tcp_server.stream.handle);
 
-        std.debug.print("bye bye\n", .{});
+            const conn = try tcp_server.accept();
+            echo(conn) catch {};
+        }
     }
-
-    // const epoll_fd = try std.posix.epoll_create1(0);
-    // const event = std.os.linux.epoll_event{
-    //     .events = std.os.linux.EPOLL.IN,
-    //     .data = .{ .fd = tcp_server.stream.handle }
-    // };
 }
